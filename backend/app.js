e593c68111c34bac6d66f53951d1589dc1a0cbea
lenuvia <83255484+lenuvia@users.noreply.github.com>
2022-11-29 18:44:47 -0500

    deploying app

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,13 @@
 
 /** Express app for PlantGreen. */
 require("dotenv").config();
+const path = require("path");
 
 const express = require("express");
 const cors = require("cors");
+const app = express();
+
+const PORT = process.env.PORT || 3001;
 
 const { NotFoundError } = require("./expressError");
 
@@ -13,12 +17,11 @@ const authRoutes = require("./routes/auth");
 const usersRoutes = require("./routes/users");
 // const plantsRoutes = require("./routes/plants");
 const apiRoutes = require("./routes/api");
-
 const morgan = require("morgan");
 
-const app = express();
-app.use(express.json({ limit: "50mb" }));
 app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
 app.use(morgan("tiny"));
 
 app.use(authenticateJWT);
@@ -42,6 +45,18 @@ app.use(function (err, req, res, next) {
   return res.status(status).json({
     error: { message, status },
   });
+});
+
+if (process.env.NODE_ENV !== "dev") {
+  // Catch-All for any request that doesn't match a route
+  // falls back to index.html
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "frontend", "build", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
 
 module.exports = app;
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -35,6 +35,7 @@ router.post("/plantdata", ensureCorrectUserOrAdmin, async function (req, res) {
   await axios
     .post("https://api.plant.id/v2/identify", data, headers)
     .then((response) => {
+      console.log("PlantID response ====>", res);
       return res.status(201).json({ data: response.data });
     })
     .catch((error) => {
