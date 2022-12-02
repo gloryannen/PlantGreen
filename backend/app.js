78ccbb2a89e848317cfeaa8b116ecc555108baf6
lenuvia <83255484+lenuvia@users.noreply.github.com>
2022-12-01 22:21:33 -0500

    deploying app

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.static(path.join(__dirname, "../frontend/build")));
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "../frontend/build/index.html"));
 });
 app.use(morgan("tiny"));
